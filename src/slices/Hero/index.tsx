02f1fe6fe476bad18a3ae9c132f96e82e311d3ac
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-01-31 15:39:38 +0530

    HERO text set

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -15,9 +15,21 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-    <>{slice.primary.first_name}</>
-    <>{slice.primary.last_name}</>
-    <>{slice.primary.tag_line}</>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1
+            className="mb-8 lg:text-[clamp(3rem,20vmin,20rem)] 
+        font-extrabold 
+        leading-none tracking-tighter 
+        
+        xl:text-[clamp(3rem,20vmin,20rem)]"
+          >
+            <span className="text-red-500">{slice.primary.first_name}</span>
+            {slice.primary.last_name}
+            {slice.primary.tag_line}
+          </h1>
+        </div>
+      </div>
     </section>
   );
 };
