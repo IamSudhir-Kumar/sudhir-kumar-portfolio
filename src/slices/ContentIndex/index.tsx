3d1c9cb926251f0d180ba903cef1842d233f45f3
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-02-16 14:57:16 +0530

    Add Projects slice and custom type

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,24 +1,49 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content , isFilled } from "@prismicio/client";
+import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import ContentList from "./ContentList";
+import { createClient } from "@/prismicio";
 
 /**
  * Props for `ContentIndex`.
  */
-export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
+export type BlogPostIndexProps =
+  SliceComponentProps<Content.BlogPostIndexSlice>;
 
 /**
  * Component for "ContentIndex" Slices.
  */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+const BlogPostIndex = async ({
+  slice,
+}: BlogPostIndexProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const projects = await client.getAllByType("project");
+
+  const items = slice.primary.content_type === "Projects" ? projects : undefined;
+
+ 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for content_index (variation: {slice.variation})
-      Slices
-    </section>
+      <Heading size="xl" className="mb-8">
+        {slice.primary.heading}
+      </Heading>
+      {isFilled.richText(slice.primary.description) && (
+        <div className="prose prose-xl prose-invert mb-10">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+      )}
+       <ContentList
+        items={items}
+        contentType={slice.primary.content_type}
+        viewMoreText={slice.primary.view_more_text}
+        fallbackItemImage={slice.primary.fallback_item_image}
+      />
+    </Bounded>
   );
 };
 
-export default ContentIndex;
+export default BlogPostIndex;
