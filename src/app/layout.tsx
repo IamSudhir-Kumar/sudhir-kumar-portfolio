731750ba9e81d0a95b5185787fc79664e0e9a69e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-02-22 15:22:26 +0530

    Add new files and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-const inter = Urbanist({ subsets: ["latin"] });
+import Footer from "@/components/Footer";
+import clsx from "clsx";
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,9 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-[#12372A] text-[#f7c291]">
-      <body className={inter.className}>
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
         {children}
+        <Footer/>
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noise-text.jpg')] opacity-20 mix-blend-soft-light">
+        </div>
+     
       </body>
     </html>
   );
